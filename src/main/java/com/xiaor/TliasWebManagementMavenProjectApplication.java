bbb81a7e3a9f5279664fbd9fd77214565424e171
yan <906596588@qq.com>
2025-02-24 10:59:30 +0800

    test tlias-web-management server successfully

---
+++ src/main/java/com/xiaor/TliasWebManagementMavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.xiaor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliasWebManagementMavenProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TliasWebManagementMavenProjectApplication.class, args);
+    }
+
+}
