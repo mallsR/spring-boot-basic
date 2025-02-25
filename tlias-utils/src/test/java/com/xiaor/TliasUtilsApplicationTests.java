39baaca88892e97267d4b953cb07500205866397
xiaoR <2471385107@qq.com>
2025-02-25 11:02:08 +0800

    split tlias-utils module

---
+++ tlias-utils/src/test/java/com/xiaor/TliasUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TliasUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
